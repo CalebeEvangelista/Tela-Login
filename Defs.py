fe424150009822d61ea8e3044854aebd876b78f8
Calebe Evangelista <calebe.leao@hotmail.com>
2021-08-18 23:27:07 -0300

    Enviando o arquivo final

--- Defs.py
+++ Defs.py
@@ -1,4 +1,34 @@
-from time import time
+import time
+from os import system
+
+def menu1():
+    system('cls')
+    print("############################################")
+    print("### 1 - Cadastrar login novo    ############")
+    print("### 2 - Fazer Login             ############")
+    print("### 0 - Sair                    ############")
+    opcao = int(input("### Digite o digito da escolha: "))
+    print("############################################")
+    time.sleep(0.5)
+    return opcao
+
+def menu2():
+    system('cls')
+    print("Você está logado!")
+    time.sleep(1)
+    escolha = input("Caso deseje sair aperte qualquer tecla!")
+    system('cls')
+    print("Saindo da sua conta .")
+    time.sleep(0.4)
+    system('cls')
+    print("Saindo da sua conta ..")
+    time.sleep(0.4)
+    system('cls')
+    print("Saindo da sua conta ...")
+    time.sleep(1)
+    system('cls')
+    print("Deslogado com sucesso!")
+    time.sleep(1)
 
 def procurarArquivo():
     try:
@@ -10,34 +40,54 @@ def procurarArquivo():
         return True
 
 def gravarArquivo(login, senha):
-    with open('login.txt' , 'a+') as arq:
-        arq.writelines(f'{login} {senha}\n')
-        arq.seek(0)
-        print(arq.readlines())
+    if buscarLogin(login) == False:
+        with open('login.txt' , 'a+') as arq:
+            arq.writelines(f'{login} {senha}\n')
+            arq.seek(0)
+            print("Usuário cadastrado com sucesso!")
         
-def buscarLogin(login, senha):
+def buscarLogin(login):
     #Primeiro você cria uma lista (logins = []), depois abre o arquivo com o 'with', em seguida para (for) cada linha no arquivo (arq) você vai dividir os argumentos com uma, e depois separar eles dentro da lista criada no começo.
     logins = []
     local = 0
     with open('login.txt' , 'r+') as arq:
         for linha in arq:
             linha = linha.strip(",")
-            logins.append(linha.split())
-            
-        print(logins)
+            logins.append(linha.split())       
         
         for dado in logins:
-            local = logins.index(login) 
-            print(logins)
-            user = dado[local]
-            password = dado[local + 1]
+            if logins[local][0] == login:
+                return True
+            else:
+                local = local + 1
+    return False
 
-            print(user)
-            print(password)
-    
-        if login == user and senha == password:
-            print("Usuário ja cadastrado!")
-    return(login, senha)
+def buscarSenha(login, senha):
+    logins = []
+    local = 0
+    with open('login.txt' , 'r+') as arq:
+        for linha in arq:
+            linha = linha.strip(",")
+            logins.append(linha.split())       
+        
+        for dado in logins:
+            if logins[local][0] == login:
+                if logins[local][1] == senha:
+                    return True
+            else:
+                local = local + 1
+    return False
 
-def cadastrarLogin(login, senha):
-    return True
+def logado(login, senha):
+    if buscarLogin(login) and buscarSenha(login,senha) == True:
+        print("Usuário logado com sucesso !")
+        return True
+    elif buscarLogin(login) == True and buscarSenha(login,senha) == False:
+        print("Senha incorreta, tente novamente !")
+        return False
+    elif buscarLogin(login) == False and buscarSenha(login,senha) == True:
+        print("Login incorreto, tente novamente!")
+        return False
+    else:
+        print("Login e senha incorretos, tente novamente ou cadastre um novo usuário!")
+        return False
--- Main.py
+++ Main.py
@@ -7,33 +7,55 @@ FUNCIONALIDADES:
 - Tratar os erros;
 '''
 
-from Defs import buscarLogin, cadastrarLogin, gravarArquivo
+from Defs import buscarLogin, buscarSenha, gravarArquivo, logado, menu1, menu2
 from os import system
 import time
+import stdiomask
+from getpass import getpass
 
 while True :
-    system('cls')
-    print("############################################")
-    print("### 1 - Cadastrar login novo    ############")
-    print("### 2 - Fazer Login             ############")
-    print("### 0 - Sair                    ############")
-    opcao = int(input("### Digite o digito da escolha: "))
-    print("############################################")
-    time.sleep(0.5)
-
-    while opcao != 0 :
+    opcao = menu1()
+    if opcao == 1:
+        #Cadastrar login novo
         system('cls')
-        if opcao == 1:
-            #Cadastrar login novo
-            login = input("Digite o login: ")
-            senha = input("Digite a senha: ")
-            cadastrarLogin(login, senha)
-            gravarArquivo(login, senha)
-            time.sleep(0.4)
-            print("Cadastrado com sucesso!")
-            time.sleep(0.5)
-            print("Voltando a tela inicial ...")
-            buscarLogin(login, senha)
-            time.sleep(0.7)
-            opcao = 0
-            time.sleep(10)
+        login = input("Digite o login: ")
+        senha = stdiomask.getpass(prompt='Digite a senha: ', mask='*')
+        if buscarLogin(login) == True:
+            print("Nome de usuário em uso, digite outro!")
+        else:
+            gravarArquivo(login,senha)
+        time.sleep(2)
+        system('cls')
+        print("Retornando para a tela inicial .")
+        time.sleep(0.4)
+        system('cls')
+        print("Retornando para a tela inicial ..")
+        time.sleep(0.4)
+        system('cls')
+        print("Retornando para a tela inicial ...")
+        time.sleep(1)
+    
+    elif opcao == 2:
+        system('cls')
+        login = input("Digite o login: ")
+        senha = stdiomask.getpass(prompt='Digite a senha: ', mask='*')
+        if logado(login, senha) == True:
+            menu2()
+    
+    elif opcao == 0:
+        system('cls')
+        print("Saindo do programa .")
+        time.sleep(0.4)
+        system('cls')
+        print("Saindo do programa ..")
+        time.sleep(0.4)
+        system('cls')
+        print("Saindo do programa ...")
+        time.sleep(1)
+        system('cls')
+        print("Até mais!")
+        time.sleep(1)
+        break
+
+    else:
+        print("Digite uma opção valida!")
